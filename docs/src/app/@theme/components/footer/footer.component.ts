f87c2144639fc99fb184e29aff4c27ae4c23d46e
sbenstewart <sbenstewart@gmail.com>
2019-11-08 19:01:56 +0530

    please push na

--- docs/src/app/@theme/components/footer/footer.component.ts
+++ docs/src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2019</span>
+    <span class="created-by">Created with ♥ by <b><a href="https://sbenstewart.in" target="_blank">
+    S Ben Stewart</a></b> 2019</span>
     <div class="socials">
       <a href="https://github.com/sbenstewart/monkey-tagger" target="_blank" class="ion ion-social-github"></a>
       <a href="https://sbenstewart.in" target="_blank" class="ion ion-tshirt-outline"></a>
