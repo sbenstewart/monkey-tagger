38ce705481a52cfa9c7fee7887fd14ce8824d054
sbenstewart <sbenstewart@gmail.com>
2019-11-08 23:11:35 +0530

    page routing added

--- docs/src/app/pages/pages-routing.module.ts
+++ docs/src/app/pages/pages-routing.module.ts
@@ -12,6 +12,10 @@ const routes: Routes = [{
   children: [
     {
       path: 'dashboard',
+      component: DashboardComponent,
+    },
+    {
+      path: 'commerce-dashboard',
       component: ECommerceComponent,
     },
     {
